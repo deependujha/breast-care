00fbfca78117e804353f6e59b3486f4c1f2acbf8
AMIT KUMAR MAHATO <miamitkumar98@gmail.com>
2022-06-05 16:35:02 +0530

    Added On Click Full View Of Story

--- server.js
+++ server.js
@@ -2,20 +2,25 @@ require('./conn/mongo')
 const express = require('express');
 const hbs = require('hbs');
 const async = require('hbs/lib/async');
-const bodyParser=require('body-parser');
-const fileUpload=require('express-fileupload');
-const path=require('path')
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const path = require('path')
 const app = express()
 const port = 5000
 
-const Stories=require('./models/stories');
+const Stories = require('./models/stories');
+const {
+  default: mongoose
+} = require('mongoose');
 
 app.set('view engine', 'hbs');
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(express.static('public'))
 app.use(fileUpload({
-  useTempFiles:true,
-  tempFileDir : path.join(__dirname,'public/tmp')
+  useTempFiles: true,
+  tempFileDir: path.join(__dirname, 'public/tmp')
 }))
 hbs.registerPartials(__dirname + '/views/partials');
 
@@ -32,9 +37,24 @@ app.get('/stories', (req, res) => {
   res.render('stories');
 })
 
-app.get('/testimonials/list',async(req,res)=>{
+
+
+app.get('/stories/list', async (req, res) => {
   try {
-    const testimonials=await Stories.find();
+    const testimonials = await Stories.find().sort({
+      createdon: -1
+    });
+    res.send(testimonials);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get('/story/id/:storyid', async (req, res) => {
+  try {
+    const testimonials = await Stories.findOne({
+      _id: req.params.storyid
+    })
     res.send(testimonials);
   } catch (error) {
     console.log(error);
@@ -43,20 +63,36 @@ app.get('/testimonials/list',async(req,res)=>{
 
 app.post('/yourstory', async (req, res) => {
   try {
-    const story=await new Stories(req.body);
-    story.save();
-    res.redirect('/stories')
+    const story = await new Stories({
+      creator: req.body.creator,
+      occupation: req.body.occupation,
+      age: req.body.age,
+      story: req.body.story,
+      createdon: Date.now()
+    });
+    var image = req.files.image;
+    var filename = "";
+    if (image.mimetype == 'image/jpeg') {
+      filename = mongoose.Types.ObjectId() + '.jpeg';
+    } else {
+      filename = mongoose.Types.ObjectId() + '.png';
+    }
+    image.mv(path.join(__dirname, "public/stories/images", filename));
+    story.imageAddress = filename;
+    console.log(story);
+    await story.save();
+    // res.redirect('/stories')
   } catch (error) {
-
+    console.log(error);
   }
 
 })
 
-app.get('/know-more',(req,res)=>{
+app.get('/know-more', (req, res) => {
   res.render('know-more');
 })
 
-app.get('/faqs',(req,res)=>{
+app.get('/faqs', (req, res) => {
   res.render('faqs');
 })
 
